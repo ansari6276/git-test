b91f273666e7bed94aac2a5be42bdb19a4c6e0c9
Your Name <you@example.com>
2020-06-16 18:53:23 +0530

    add dto for method argument

--- src/main/java/com/example/git/app/GitTestAppApplication.java
+++ src/main/java/com/example/git/app/GitTestAppApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -11,9 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class GitTestAppApplication {
 
 	@PostMapping("/purchase/{username}/{amount}/{productName}")
-	public String purchase(@PathVariable String username, @PathVariable double amount,
-			@PathVariable String productName) {
-		return "Hi "+username+" order for "+productName+" with amount "+amount+" stored successfully";
+	public String purchase(@RequestBody Order order) {
+		return "Hi "+order.getUsername()+" order for "+order.getProductName()+" with amount "+order.getAmount()+" stored successfully";
 	}
 
 	public static void main(String[] args) {
